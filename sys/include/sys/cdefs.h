54cc4aeea9d03cf65ffe211448231b9ac52cb1c8
Ian Marco Moffett <ian@mkall.org>
2023-05-15 19:30:21 -0400

    kernel: Add __packed to cdefs.h

--- sys/include/sys/cdefs.h
+++ sys/include/sys/cdefs.h
@@ -32,6 +32,7 @@
 
 #define __dead     __attribute__((__noreturn__))
 #define __used     __attribute__((__used__))
+#define __packed   __attribute__((__packed__))
 #define __unused   __attribute__((__unused__))
 #define __atomic   _Atomic
 
