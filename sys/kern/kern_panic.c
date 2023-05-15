e1e0392b22e5c7e084900b3d13f9146a9f2f3639
Ian Marco Moffett <ian@mkall.org>
2023-05-15 17:42:34 -0400

    kernel: Add panic()

---
+++ sys/kern/kern_panic.c
@@ -0,0 +1,160 @@
+/*
+ * Copyright (c) 2023 Ian Marco Moffett and the VegaOS team.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of VegaOS nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE         
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/panic.h>
+#include <sys/types.h>
+#include <sys/printk.h>
+#include <vt/vt.h>
+
+#define MAX_FRAMES 15
+
+#define SCREEN_BG 0x000080
+#define SCREEN_FG 0xF2F5E5
+#define ASCII_SEP "----------------------------------" \
+                  "----------------------------------"
+
+extern struct vt_descriptor g_vt;
+
+struct kernel_symbol {
+	uint64_t addr;
+	char* name;
+};
+
+extern struct kernel_symbol g_ksym_table[];
+
+/*
+ * Does a stacktrace.
+ *
+ * TODO: Add support with other arches.
+ */
+
+#if defined(__x86_64__)
+
+static const char *
+panic_resolve_name(uintptr_t addr, size_t *offset)
+{
+    uintptr_t prev_addr = 0;
+    const char *prev_name = NULL;
+
+    for (size_t i = 0;; ++i) {
+        if (g_ksym_table[i].addr > addr) {
+            *offset = addr - prev_addr;
+            return prev_name;
+        }
+
+        prev_addr = g_ksym_table[i].addr;
+        prev_name = g_ksym_table[i].name;
+
+        if (g_ksym_table[i].addr == (uintptr_t)-1) {
+            return NULL;
+        }
+    }
+
+    __builtin_unreachable();
+}
+
+static void
+panic_stacktrace(void)
+{
+    uintptr_t *rbp = NULL;
+    __asm("mov %%rbp, %0"
+          : "=r" (rbp)
+          :
+          : "memory"
+    );
+
+    size_t frame_count = 0; 
+    size_t offset;
+
+    while (rbp != NULL && frame_count < MAX_FRAMES) {
+        const char *name = panic_resolve_name(rbp[1], &offset);
+
+        if (name == NULL) {
+            printk("* [%d] 0x%x\n", frame_count, rbp[1]);
+        } else {
+            printk("* [%d] %s <0x%x+0x%x>\n",
+                   frame_count, name, rbp[1], offset);
+        }
+
+        rbp = (uintptr_t *)rbp[0];
+        ++frame_count;
+    }
+}
+#endif
+
+/*
+ * Writes out diagnostic information
+ * to the console.
+ */
+
+static void
+panic_log_diag(const char *fmt, va_list ap)
+{
+    printk("\n\nSomething went wrong with Vega "
+           "and your PC requires a restart.\n"); 
+
+    printk("Below is some technical information, "
+            "if you aren't a developer you can\n"
+            "either send us a picture or ignore this.\n\n");
+
+    printk("** Technical information **\n\n");
+    
+#if defined(__x86_64__)
+    printk("Traversing call stack...\n");
+    printk("%s\n", ASCII_SEP);
+
+    panic_stacktrace();
+    printk("%s\n\n", ASCII_SEP);
+#endif
+    
+    printk("Vega revision: v%s\n\n", VEGA_VERSION);
+
+    printk("kpanic - halting: "); 
+    vprintk(fmt, ap);
+}
+
+__dead void
+panic(const char *fmt, ...)
+{
+    struct vt_attr panic_attr = {
+        .bg = SCREEN_BG,
+        .text_bg = SCREEN_BG,
+        .text_fg = SCREEN_FG,
+        .cursor_type = CURSOR_NONE
+    };
+    
+    vt_chattr(&g_vt, &panic_attr);
+    vt_reset(&g_vt);
+
+    va_list ap;
+    va_start(ap, fmt);
+    panic_log_diag(fmt, ap);
+
+    for (;;);
+}
