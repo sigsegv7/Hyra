b09c9e3853162580d4bdd3eb120f9987397a956d
Ian Marco Moffett <ian@mkall.org>
2023-05-15 14:16:10 -0400

    kern: Move bool to stdbool.h

--- sys/include/lib/stdbool.h
+++ sys/include/lib/stdbool.h
@@ -27,57 +27,12 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef _SYS_TYPES_H_
-#define _SYS_TYPES_H_
+#ifndef _LIB_STDBOOL_H_
+#define _LIB_STDBOOL_H_
 
-#include <stdbool.h>
+#define true 1
+#define false 0
 
-#if !defined(NULL)
-# define NULL (void *)0
+typedef _Bool           bool;
+
 #endif
-
-typedef signed char     int8_t;
-typedef unsigned char   uint8_t;
-typedef short           int16_t;
-typedef unsigned short  uint16_t;
-typedef int             int32_t;
-typedef unsigned int    uint32_t;
-#if __SIZEOF_LONG__ == 8
-typedef long            int64_t;
-typedef unsigned long   uint64_t;
-#elif __SIZEOF_LONG__ == 4
-__extension__
-typedef long long       int64_t;
-__extension__
-typedef unsigned long long uint64_t;
-#else
-#error "Unsupported long size"
-#endif
-
-#if __SIZEOF_SIZE_T__ == 8
-typedef uint64_t        size_t;
-typedef int64_t  ssize_t;       /* Byte count or error */
-#elif __SIZEOF_SIZE_T__ == 4
-typedef uint32_t        size_t;
-typedef int32_t         ssize_t;        /* Byte count or error */
-#else
-#error "Unsupported size_t size"
-#endif
-
-typedef size_t          uintptr_t;
-#ifndef __ptrdiff_defined
-typedef size_t          ptrdiff_t;
-#endif
-typedef size_t          off_t;
-
-typedef size_t          id_t;
-typedef id_t            pid_t;
-typedef id_t            tid_t;
-typedef id_t            uid_t;
-typedef id_t            gid_t;
-typedef uint32_t        ino_t;
-typedef int32_t         dev_t;
-typedef int32_t         mode_t;
-typedef int32_t         nlink_t;
-
-#endif   /* _SYS_TYPES_H_ */
