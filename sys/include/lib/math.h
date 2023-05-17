963b70b99d4ec7ae6b59408862185277d3f255e5
Ian Marco Moffett <ian@mkall.org>
2023-05-17 12:28:16 -0400

    kernel/lib: Add math lib

---
+++ sys/include/lib/math.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2023 Ian Marco Moffett and the VegaOS team.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of VegaOS nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _LIB_MATH_H_
+#define _LIB_MATH_H_
+
+#define ALIGN_DOWN(address, align)  ((address) & ~((align)-1))
+#define ALIGN_UP(address, align)    (((address) + (align)-1) & ~((align)-1))
+#define IS_ALIGNED(address, align) ((address & (align - 1)) == 0)
+
+#define MAX(A, B)  __extension__ ({ \
+    __auto_type MAX_a = A; \
+    __auto_type MAX_b = B; \
+    MAX_a > MAX_b ? MAX_a : MAX_b; \
+})
+
+#define MIN(A, B) __extension__ ({ \
+    __auto_type MAX_a = A; \
+    __auto_type MAX_b = B; \
+    MAX_a < MAX_b ? MAX_a : MAX_b; \
+})
+
+#define DIV_ROUNDUP(VALUE, DIV) __extension__ ({ \
+    __auto_type DIV_ROUNDUP_value = VALUE; \
+    __auto_type DIV_ROUNDUP_div = DIV; \
+    (DIV_ROUNDUP_value + (DIV_ROUNDUP_div - 1)) / DIV_ROUNDUP_div; \
+})
+
+#endif
