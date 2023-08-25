8eae72a7cfa86bcbe11f7fa15f1bbfaf3eadb7ad
emilia <emilia@vegaa.systems>
2023-08-25 04:13:07 +0000

    kernel: Add MMIO helpers
    Allows MMIO to be preformed more concisely while be less
    error prone
    
    
    
    git-svn-id: https://svn.vegaa.systems/svn/vega-Vega/trunk@35 a8a8aea2-181d-ee11-89e8-15fd0e089fc4

---
+++ sys/include/sys/mmio.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2023 Emilia Strange and the VegaOS team.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of VegaOS nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* $Id$ */
+
+#ifndef _SYS_MMIO_H_
+#define _SYS_MMIO_H_
+
+#define _MMIO_WRITE_TYPE(TYPE, SUFFIX)                  \
+    static inline void                                  \
+    mmio_write##SUFFIX(uintptr_t addr, TYPE val)        \
+    {                                                   \
+        *(volatile TYPE *)addr = val;                   \
+    }
+
+/*
+ * To write to an MMIO address of, for example,
+ * 8 bits, use mmio_write8(addr, val)
+ */
+_MMIO_WRITE_TYPE(uint8_t, 8)
+_MMIO_WRITE_TYPE(uint16_t, 16)
+_MMIO_WRITE_TYPE(uint32_t, 32)
+#if __SIZEOF_SIZE_T__ == 8
+_MMIO_WRITE_TYPE(uint64_t, 64)
+#endif
+__extension__
+
+#endif  /* !_SYS_MMIO_H_ */
