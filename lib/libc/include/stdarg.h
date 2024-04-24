0dd63753b6e8c3cbcc1a790c3c6ecd40d8320048
Ian Moffett <ian@osmora.org>
2024-04-24 19:42:06 -0400

    libc: stdarg: Fix va_end()
    Signed-off-by: Ian Moffett <ian@osmora.org>

--- lib/libc/include/stdarg.h
+++ lib/libc/include/stdarg.h
@@ -35,7 +35,7 @@
 typedef __builtin_va_list va_list;
 
 #define va_start(v, l) __builtin_va_start(v, l)
-#define va_end(v, l) __builtin_va_end(v, l)
+#define va_end(v) __builtin_va_end(v)
 #define va_arg(v, l) __builtin_va_arg(v, l)
 #define va_copy(v, l) __builtin_va_copy(v, l)
 
