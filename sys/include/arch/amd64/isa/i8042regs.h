bfb2bce1a7b4cb623e2ace5b6b2fa6718bd6e2bc
Ian Moffett <ian@osmora.org>
2024-05-12 19:41:05 -0400

    kernel/amd64: isa: Add i8042 driver
    Signed-off-by: Ian Moffett <ian@osmora.org>

--- sys/include/arch/amd64/isa/i8042regs.h
+++ sys/include/arch/amd64/isa/i8042regs.h
@@ -27,28 +27,43 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef _AMD64_SYSVEC_H_
-#define _AMD64_SYSVEC_H_
+#ifndef _AMD64_I8042REG_H_
+#define _AMD64_I8042REG_H_
 
 #include <sys/cdefs.h>
 
-/*
- * Interrupt vectors reserved
- * to core system usage excluding
- * device IRQs routed by 8259 PIC,
- * I/O APIC, etc.
- */
-typedef enum {
-    SYSVEC_LAPIC_TIMER = 0x21,     /* Local APIC timer */
-    SYSVEC_IPI,                    /* IPI vector */
-    SYSVEC_HLT,                    /* Halt vector */
-    SYSVEC_PCKBD,                  /* Keyboard vector */
+#define I8042_DATA      0x60
+#define I8042_STATUS    0x64
+#define I8042_COMMAND   0x64
 
-    /* -- XXX: New vectors go above -- */
-    NSYSVEC_BASE,                  /* Non-system vector base */
-} sysvec_t;
+/* Status register bits */
+#define I8042_OBUF_FULL     __BIT(0)    /* Output buffer full */
+#define I8042_IBUF_FULL     __BIT(1)    /* Input buffer full */
 
-/* Unlikely, but just in case */
-__STATIC_ASSERT(NSYSVEC_BASE <= 0xFF, "VECTOR OVERFLOW CAUGHT");
+/* Commands */
+#define I8042_DISABLE_PORT0 0xAD
+#define I8042_DISABLE_PORT1 0xA7
+#define I8042_ENABLE_PORT0  0xAE
+#define I8042_ENABLE_PORT1  0xA8
+#define I8042_GET_CONFB     0x20
+#define I8042_SET_CONFB     0x60
+#define I8042_PORT1_SEND    0xD4
 
-#endif  /* !_AMD64_SYSVEC_H_ */
+/* Controller config bits */
+#define I8042_PORT0_INTR    __BIT(0)
+#define I8042_PORT1_INTR    __BIT(1)
+#define I8042_PORT0_CLK     __BIT(4)
+#define I8042_PORT1_CLK     __BIT(5)
+
+/* Aux commands */
+#define I8042_AUX_DEFAULTS 0xF5
+#define I8042_AUX_ENABLE   0xF4
+#define I8042_AUX_DISABLE  0xF5
+#define I8042_AUX_RESET    0xFF
+
+/* LED bits */
+#define I8042_LED_SCROLL    __BIT(0)
+#define I8042_LED_NUM       __BIT(1)
+#define I8042_LED_CAPS      __BIT(2)
+
+#endif  /* !_AMD64_I8042REG_H_ */
