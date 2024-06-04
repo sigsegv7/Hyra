54096dae0fd8ae38161caa27c76355657a1bcf84
Ian Moffett <ian@osmora.org>
2024-06-04 14:56:28 -0400

    kernel: lib: Add memcmp()
    Signed-off-by: Ian Moffett <ian@osmora.org>

--- sys/lib/string/memcmp.c
+++ sys/lib/string/memcmp.c
@@ -27,19 +27,19 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef _LIB_STRING_H_
-#define _LIB_STRING_H_
+#include <string.h>
 
-#include <sys/types.h>
-#include <stdarg.h>
+int
+memcmp(const void *s1, const void *s2, size_t n)
+{
+	if (n != 0) {
+        const unsigned char *p1 = s1, *p2 = s2;
 
-size_t strlen(const char *s);
-char *itoa(int64_t value, char *buf, int base);
-
-int vsnprintf(char *s, size_t size, const char *fmt, va_list ap);
-int snprintf(char *s, size_t size, const char *fmt, ...);
-
-void *memcpy(void *dest, const void *src, size_t n);
-int memcmp(const void *s1, const void *s2, size_t n);
-
-#endif  /* !_LIB_STRING_H_ */
+		do {
+			if (*p1++ != *p2++) {
+				return (*--p1 - *--p2);
+            }
+		} while (--n != 0);
+    }
+	return 0;
+}
