7221137c81b87a25c800b0aa186f0c2a566355d0
Ian Moffett <ian@osmora.org>
2024-06-13 18:30:55 -0400

    kernel: string: Add strcmp()
    Signed-off-by: Ian Moffett <ian@osmora.org>

--- sys/lib/string/strcmp.c
+++ sys/lib/string/strcmp.c
@@ -27,22 +27,16 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef _LIB_STRING_H_
-#define _LIB_STRING_H_
+#include <string.h>
 
-#include <sys/types.h>
-#include <stdarg.h>
+int
+strcmp(const char *s1, const char *s2)
+{
+    while (*s1 == *s2++) {
+        if (*s1++ == 0) {
+            return (0);
+        }
+    }
 
-size_t strlen(const char *s);
-char *itoa(int64_t value, char *buf, int base);
-
-int vsnprintf(char *s, size_t size, const char *fmt, va_list ap);
-int snprintf(char *s, size_t size, const char *fmt, ...);
-
-void *memcpy(void *dest, const void *src, size_t n);
-int memcmp(const void *s1, const void *s2, size_t n);
-
-void *memset(void *s, int c, size_t n);
-int strcmp(const char *s1, const char *s2);
-
-#endif  /* !_LIB_STRING_H_ */
+    return (*(unsigned char *)s1 - *(unsigned char *)--s2);
+}
