3ef807f071feaffef409e1817d781a9d49584b5c
Ian Moffett <ian@osmora.org>
2024-06-30 22:21:19 -0400

    kernel: cdefs: Add __dead
    Signed-off-by: Ian Moffett <ian@osmora.org>

--- sys/include/sys/cdefs.h
+++ sys/include/sys/cdefs.h
@@ -35,6 +35,7 @@
 #define __ASMV          __asm__ __volatile__
 #define __always_inline __attribute__((__always_inline__))
 #define __packed        __attribute__((__packed__))
+#define __dead          __attribute__((__noreturn__))
 #define __likely(exp)   __builtin_expect(((exp) != 0), 1)
 #define __unlikely(exp) __builtin_expect(((exp) != 0), 0)
 #define __static_assert _Static_assert
