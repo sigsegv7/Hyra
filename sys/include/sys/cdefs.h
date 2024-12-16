3fea90b0047f920c8b70e1d3d306d8afe0f99983
Ian Moffett <ian@osmora.org>
2024-12-15 21:48:04 -0500

    kernel: Add __unused attribute to cdefs.h
    Signed-off-by: Ian Moffett <ian@osmora.org>

--- sys/include/sys/cdefs.h
+++ sys/include/sys/cdefs.h
@@ -36,6 +36,7 @@
 #define __always_inline __attribute__((__always_inline__))
 #define __packed        __attribute__((__packed__))
 #define __dead          __attribute__((__noreturn__))
+#define __unused        __attribute__((__unused__))
 #define __likely(exp)   __builtin_expect(((exp) != 0), 1)
 #define __unlikely(exp) __builtin_expect(((exp) != 0), 0)
 #define __static_assert _Static_assert
