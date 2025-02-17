2fe2d7b0a3cf6ee72debefa954c12093da3cac81
Ian Moffett <ian@osmora.org>
2025-02-16 21:57:02 -0500

    project: Update copyright date to 2025
    Was supposed to happen on Jan 1, sorry! Happy late new year!
    
    Signed-off-by: Ian Moffett <ian@osmora.org>

--- sys/arch/amd64/amd64/intr.c
+++ sys/arch/amd64/amd64/intr.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023-2024 Ian Marco Moffett and the Osmora Team.
+ * Copyright (c) 2023-2025 Ian Marco Moffett and the Osmora Team.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
