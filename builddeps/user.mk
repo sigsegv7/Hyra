e874d10025a1b19583c4b11a71b06655cea5b6ed
Ian Moffett <ian@osmora.org>
2025-04-18 21:43:56 -0400

    usr: Make gcc put less crap in user binaries
    Signed-off-by: Ian Moffett <ian@osmora.org>

--- builddeps/user.mk
+++ builddeps/user.mk
@@ -1,6 +1,7 @@
-CC =
+CC = gcc
 LIBDIR =
 USRDIR =
 LDSCRIPT =
 INTERNAL_CFLAGS = -T$(LDSCRIPT) -znoexecstack -nostdlib -I$(USRDIR)/include/ \
-				  -L$(USRDIR)/lib -lc
+				  -L$(USRDIR)/lib -lc -pie -no-pie -fno-stack-protector \
+				  -fno-asynchronous-unwind-tables
