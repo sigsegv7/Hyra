19497ac00ceb8e225ce7008df26024d4c6a1d2b9
Ian Moffett <ian@osmora.org>
2025-05-31 17:17:13 -0400

    usr: Pass OS version and ARCH to user programs
    Signed-off-by: Ian Moffett <ian@osmora.org>

--- builddeps/user.mk
+++ builddeps/user.mk
@@ -4,4 +4,5 @@ USRDIR =
 LDSCRIPT =
 INTERNAL_CFLAGS = -T$(LDSCRIPT) -znoexecstack -nostdlib -I$(USRDIR)/include/ \
 				  -L$(USRDIR)/lib -lc -pie -no-pie -fno-stack-protector \
-				  -fno-asynchronous-unwind-tables
+				  -fno-asynchronous-unwind-tables -D_OSVER=\"$(OSVER)\" \
+				  -D_OSARCH=\"$(OSARCH)\"
