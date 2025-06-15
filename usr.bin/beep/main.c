bc9959c25e9f22ae73a34ac6c4c9cd9cae8ebeca
Ian Moffett <ian@osmora.org>
2025-06-15 17:35:26 -0400

    usr.bin: Add beep command
    Signed-off-by: Ian Moffett <ian@osmora.org>

---
+++ usr.bin/beep/main.c
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2023-2025 Ian Marco Moffett and the Osmora Team.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of Hyra nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int
+main(int argc, char **argv)
+{
+    uint32_t payload;
+    uint16_t duration;
+    uint16_t freq;
+    int beep_fd;
+
+    if (argc < 3) {
+        printf("usage: beep <freq> <duration>\n");
+        return -1;
+    }
+
+    duration = atoi(argv[2]);
+    freq = atoi(argv[1]);
+
+    if (duration == 0) {
+        printf("bad duration\n");
+        return -1;
+    }
+    if (freq == 0) {
+        printf("bad frequency\n");
+        return -1;
+    }
+
+    beep_fd = open("/dev/beep", O_WRONLY);
+    if (beep_fd < 0) {
+        printf("failed to open beep fd\n");
+        return -1;
+    }
+
+    payload = duration;
+    payload |= (freq << 16);
+    write(beep_fd, &payload, sizeof(payload));
+    return 0;
+}
