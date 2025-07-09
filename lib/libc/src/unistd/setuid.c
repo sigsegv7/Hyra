9e54e6bac1b50e42cd510f16859c4d480f402144
Ian Moffett <ian@osmora.org>
2025-07-09 01:07:59 -0400

    usr: libc: Add setuid()
    Signed-off-by: Ian Moffett <ian@osmora.org>

--- lib/libc/src/unistd/setuid.c
+++ lib/libc/src/unistd/setuid.c
@@ -27,36 +27,11 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef _UNISTD_H
-#define _UNISTD_H
+#include <sys/syscall.h>
+#include <unistd.h>
 
-#include <sys/exec.h>
-#include <sys/types.h>
-#include <sys/cdefs.h>
-#include <stddef.h>
-
-#define F_OK 0
-
-/* lseek whence, follows Hyra ABI */
-#define SEEK_SET 0
-#define SEEK_CUR 1
-#define SEEK_END 2
-
-__BEGIN_DECLS
-
-int sysconf(int name);
-int setuid(uid_t new);
-
-ssize_t read(int fd, void *buf, size_t count);
-ssize_t write(int fd, const void *buf, size_t count);
-
-int close(int fd);
-int access(const char *path, int mode);
-off_t lseek(int fildes, off_t offset, int whence);
-
-pid_t getpid(void);
-pid_t getppid(void);
-
-__END_DECLS
-
-#endif  /* !_UNISTD_H */
+int
+setuid(uid_t new)
+{
+    return syscall(SYS_setuid, new);
+}
