fa85c2c767b72c5e870152934ce30049a8157cb6
Ian Moffett <ian@osmora.org>
2025-08-08 17:26:16 -0400

    lib: libc: Add POSIX getcwd() stub
    Signed-off-by: Ian Moffett <ian@osmora.org>

--- lib/libc/src/unistd/getcwd.c
+++ lib/libc/src/unistd/getcwd.c
@@ -27,55 +27,15 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef _UNISTD_H
-#define _UNISTD_H
+#include <unistd.h>
 
-#include <sys/exec.h>
-#include <sys/types.h>
-#include <sys/cdefs.h>
-#include <stddef.h>
+char *
+getcwd(char *buf, size_t size)
+{
+    if (buf == NULL) {
+        return NULL;
+    }
 
-#define F_OK 0
-
-/* lseek whence, follows Hyra ABI */
-#define SEEK_SET 0
-#define SEEK_CUR 1
-#define SEEK_END 2
-
-__BEGIN_DECLS
-
-int sysconf(int name);
-int setuid(uid_t new);
-
-int gethostname(char *name, size_t size);
-int sethostname(const char *name, size_t size);
-
-uid_t getuid(void);
-char *getlogin(void);
-char *getcwd(char *buf, size_t size);
-
-ssize_t read(int fd, void *buf, size_t count);
-ssize_t write(int fd, const void *buf, size_t count);
-
-int close(int fd);
-int access(const char *path, int mode);
-
-off_t lseek(int fildes, off_t offset, int whence);
-int unlinkat(int dirfd, const char *pathname, int flags);
-int unlink(const char *path);
-
-int dup(int fd);
-int dup2(int fd, int fd1);
-
-pid_t getpid(void);
-pid_t getppid(void);
-pid_t fork(void);
-
-extern char *optarg;
-extern int optind, opterr, optopt;
-
-int getopt(int argc, char *argv[], const char *optstring);
-
-__END_DECLS
-
-#endif  /* !_UNISTD_H */
+    /* TODO: STUB */
+    return NULL;
+}
